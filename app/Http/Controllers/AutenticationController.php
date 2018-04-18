d87f2a15f01a832d672aa4a3a322c4893ffbd74f
dossantoscarlos <anjoazrael10@gmail.com>
2018-04-18 00:23:03 -0300

    login

---
+++ app/Http/Controllers/AutenticationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AutenticationController extends Controller
+{
+    public function __construct(){
+
+    }
+
+
+    public function index(){
+    	return view('dashboard');
+    }
+
+
+    public function select(Request $request){
+
+    	if ($request->isMethod('post')){
+    		return view('dashboard');
+    	}
+    	//return view('dashboard');
+    }
+
+
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.app')
+@section('title' , 'Dashboard')
+@section('dashboard')
+	<div id='login'>
+		<form method="POST" accept-charset="utf-8" enctype="utf-8" action="/users/login">
+			<div class="columnDireita">
+				<div class="component" style="text-align: center; ">
+					<label style="font-size: 18pt;font-stretch: expanded"><b>Login-In</b></label>
+				</div>
+				<div class="component">
+					<input type="text" placeholder="Usuario" name="users" required title='Campo de Usuario requerido' minlength="10" maxlength="10" />
+				</div>
+				<div class="component" style="display:none">
+					@csrf
+				</div>
+				<div class="component">
+					<input type="password" name="pass" required="true" minlength="6" maxlength="10" title='Campo de Senha requerido' placeholder="********" />
+				</div>
+
+				<div class="component">
+					<a href="#">Recuperar senha</a>
+				</div>
+
+				<div class="component">
+					<input type="submit" name="iniciar_sessao" value="Iniciar Sessão" />
+				</div>
+			</div>
+		</form>
+	</div>
+@endsection
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+			alert('teste');
+		}
+	);
+
+</script>
+<style type="text/css">
+	*{
+		padding: 0;
+		margin: 0 auto;
+	}
+
+	.columnDireita{
+		margin-top: 0 auto;
+		margin-top: 10%;
+		max-width: 400px;
+	}
+
+	#login{
+		margin:0 auto;
+		margin-top:6%;
+		min-width: 600px;
+		max-width: 700px;
+		border:2px solid;
+		border-radius: 10px;
+		height: <?php 
+			function height($valor){
+				return ($valor/2 + 100);
+			}
+			echo height(700);
+		?>px;
+	}
+
+	.component{
+		padding: 12px;
+	}
+	input[type='text']:hover,input[type='password']:hover{
+		border-color: #FCCFCF;
+		border-radius: 10px 10px 10px 10px;
+	}
+
+	input[type='text'],input[type='password']{
+		width: 400px;
+		padding: 10px;
+		font-size: 12pt;
+		border-radius: 10px 10px 10px 10px;
+		border:2px solid #CCCCCC;
+	}
+
+	input[type='submit']{
+		border-radius: 10px 10px 10px 10px;
+		font-size: 14px;
+		color:#FFFF;
+		padding: 12px;
+		border: none;
+		box-shadow: 2px 2px 2px 2px #CCCCCC;
+		background-color:blue;
+	}
+</style>
