3e26791a1367cfb65b15b4f5165bf9b9ba51718b
dossantoscarlos <anjoazrael10@gmail.com>
2018-04-22 15:47:39 -0300

    corrigindo enderecos uris

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ponto-de-venda.herokuapp.com/public/'),
+    'url' => env('APP_URL', 'http://ponto-de-venda.herokuapp.com/public/'),
 
     /*
     |--------------------------------------------------------------------------
