b2a46ce4c1227c07de4887422865cf06ab7588c7
dossantoscarlos <anjoazrael10@gmail.com>
2018-05-05 22:45:41 -0300

    redefinindo layout para offline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <title>@yield('title','Home')</title>
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
