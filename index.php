375a24130e31ec3c0234b3e2404e4594b58825aa
dossantoscarlos <anjoazrael10@gmail.com>
2018-05-21 23:44:45 -0300

    atualizacao do login

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-<!-- <script>
+<script type="text/javascript">
 (function (){
   var href = location.href;
   window.location = href+'public/';
 })();
 </script>
- -->
-
-<?php 
-  $url = (isset($_SERVE['HTTP_REFERER'])) $_SERVE['HTTP_REFERER']: '/public/';
-  $index = '/public/';
-  if($url != $index){
-  	$concatena  = $url.$index;
-  	header('Location:'.$concatena);
-  }else{
- 	header('Location:'.$url);
-  }
-  
-  
-?>
-
+<!--
+<?php
+  // $url = (isset($_SERVE['HTTP_REFERER'])) $_SERVE['HTTP_REFERER']: '/public/';
+  // $index = '/public/';
+  // if($url != $index){
+  // 	$concatena  = $url.$index;
+  // 	header('Location:'.$concatena);
+  // }else{
+ 	// header('Location:'.$url);
+  // }
+?> -->
